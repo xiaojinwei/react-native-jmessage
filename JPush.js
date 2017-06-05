b48c7b59b9e594fbc49a9b70b28dbab1b4eb9889
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2017-06-05 09:52:36 +0800

    调整极光推送的api

--- JPush.js
+++ JPush.js
@@ -1,278 +1,61 @@
-import {
-    AppRegistry,
+
+import React, {
     NativeModules,
+    DeviceEventEmitter, //android
+    NativeAppEventEmitter, //ios
     Platform,
-    DeviceEventEmitter
+    AppState,
 } from 'react-native';
 
 import headlessJsTask from './headlessTask';
 
 const JPushModule = NativeModules.JPushModule;
-const HEADLESS_TASK = "headlessJsTask";
-const listeners = {};
-const receiveCustomMsgEvent = "receivePushMsg";
-const receiveNotificationEvent = "receiveNotification";
-const openNotificationEvent = "openNotification";
-const getRegistrationIdEvent = "getRegistrationId";
+var receiveMessageSubscript, openMessageSubscription;
 
-AppRegistry.registerHeadlessTask(HEADLESS_TASK, () => headlessJsTask);
+module.exports = JPush= {
+    // setAppkeyAndSecret(key:String,secret:String) {
+    //     JPushModule.setAppkeyAndSecret(key,secret);
+    // },
+    // addAlias(alias:String,type:String) {
+    //     JPushModule.addAlias(alias,type);
+    // },
+    setAlias(alias:String) {
+        JPushModule.setAlias(alias);
+    },
 
-/**
- * Logs message to console with the [JPush] prefix
- * @param  {string} message
- */
-const log = (message) => {
-    console.log(`[JPush] ${message}`);
-}
-// is function
-const isFunction = (fn) => typeof fn === 'function';
-/**
- * create a safe fn env
- * @param  {any} fn
- * @param  {any} success
- * @param  {any} error
- */
-const safeCallback = (fn, success, error) => {
+    removeAlias(alias:String) {
+        JPushModule.removeAlias(alias);
+    },
 
-    JPushModule[fn](function(params) {
-        log(params);
-        isFunction(success) && success(params)
-    }, function(error) {
-        log(error)
-        isFunction(error) && error(error)
-    })
+    getDeviceToken(handler: Function) {
+        JPushModule.getDeviceToken(handler);
+    },
 
-}
-
-export default class JPush {
-
-    /**
-     * Android only
-     * 初始化JPush 必须先初始化才能执行其他操作
-     */
-    static initPush() {
-        JPushModule.initPush();
-    }
-
-    /**
-     * Android
-     */
-    static stopPush() {
-        JPushModule.stopPush();
-    }
-
-    /**
-     * Android
-     */
-    static resumePush() {
-        JPushModule.resumePush();
-    }
-
-    /**
-     * Android
-     */
-    static clearAllNotifications() {
-        JPushModule.clearAllNotifications();
-    }
-
-    /**
-     * Android
-     */
-    static clearNotificationById(id) {
-        JPushModule.clearNotificationById(id);
-    }
-
-    /**
-     * Android
-     */
-    static getInfo(cb) {
-        JPushModule.getInfo((map) => {
-            cb(map);
-        });
-    }
-
-    static setTags(tag, success, fail) {
-        JPushModule.setTags(tag, (resultCode) => {
-
-            if (resultCode === 0) {
-                console.log('success');
-                success();
-            } else {
-                console.log('fail');
-                fail();
+    didReceiveMessage(handler: Function) {
+        receiveMessageSubscript = this.addEventListener(JPushModule.DidReceiveMessage, message => {
+            //处于后台时，拦截收到的消息
+            if(AppState.currentState === 'background') {
+                return;
             }
+            handler(message);
         });
-    }
+    },
 
-    static setAlias(alias, success, fail) {
-        JPushModule.setAlias(alias, (resultCode) => {
-            if (resultCode === 0) {
-                success();
-            } else {
-                fail();
-            }
-        });
-    }
+    didOpenMessage(handler: Function) {
+        openMessageSubscription = this.addEventListener(JPushModule.DidOpenMessage, handler);
+    },
 
-    static setAliasAndTags(alias, tag, success, fail) {
-        JPushModule.setAliasAndTags(alias, tag, (resultCode) => {
-            if (resultCode === 0) {
-                success();
-            } else {
-                fail();
-            }
-        });
-    }
-
-    /**
-     * Android
-     */
-    static setStyleBasic() {
-        JPushModule.setStyleBasic();
-    }
-
-    /**
-     * Android
-     */
-    static setStyleCustom() {
-        JPushModule.setStyleCustom();
-    }
-
-    /**
-     * Android
-     */
-    static addReceiveCustomMsgListener(cb) {
-        listeners[cb] = DeviceEventEmitter.addListener(receiveCustomMsgEvent,
-            (message) => {
-                cb(message);
+    addEventListener(eventName: string, handler: Function) {
+        if(Platform.OS === 'android') {
+            return DeviceEventEmitter.addListener(eventName, (event) => {
+                handler(event);
             });
-    }
-
-    /**
-     * Android
-     */
-    static removeReceiveCustomMsgListener(cb) {
-        if (!listeners[cb]) {
-            return;
         }
-        listeners[cb].remove();
-        listeners[cb] = null;
-    }
-
-    /**
-     * Android
-     */
-    static addReceiveNotificationListener(cb) {
-        listeners[cb] = DeviceEventEmitter.addListener(receiveNotificationEvent,
-            (map) => {
-                cb(map);
-            });
-    }
-
-    /**
-     * Android
-     */
-    static removeReceiveNotificationListener(cb) {
-        if (!listeners[cb]) {
-            return;
+        else {
+            return NativeAppEventEmitter.addListener(
+                eventName, (userInfo) => {
+                    handler(userInfo);
+                });
         }
-        listeners[cb].remove();
-        listeners[cb] = null;
-    }
-
-    /**
-     * Android
-     */
-    static addReceiveOpenNotificationListener(cb) {
-        listeners[cb] = DeviceEventEmitter.addListener(openNotificationEvent,
-            (message) => {
-                cb(message);
-            });
-    }
-
-    /**
-     * Android
-     */
-    static removeReceiveOpenNotificationListener(cb) {
-        if (!listeners[cb]) {
-            return;
-        }
-        listeners[cb].remove();
-        listeners[cb] = null;
-    }
-
-    /**
-     * Android
-     * If device register succeed, the server will return registrationId
-     */
-    static addGetRegistrationIdListener(cb) {
-        listeners[cb] = DeviceEventEmitter.addListener(getRegistrationIdEvent,
-            (registrationId) => {
-                cb(registrationId);
-            });
-    }
-
-    static removeGetRegistrationIdListener(cb) {
-        if (!listeners[cb]) {
-            return;
-        }
-        listeners[cb].remove();
-        listeners[cb] = null;
-    }
-
-    /**
-     * iOS,  Android
-     */
-    static getRegistrationID(cb) {
-        JPushModule.getRegistrationID((id) => {
-            cb(id);
-        });
-    }
-
-    /**
-     * iOS
-     */
-    static setupPush() {
-        JPushModule.setupPush();
-    }
-
-    /**
-     * iOS
-     */
-    static getAppkeyWithcallback(cb) {
-        JPushModule.getAppkeyWithcallback((appkey) => {
-            cb(appkey);
-        });
-    }
-
-
-    /**
-     * iOS
-     */
-    static setLocalNotification(date, textContain, badge, alertAction, notificationKey, userInfo, soundName) {
-        JPushModule.setLocalNotification(date, textContain, badge, alertAction, notificationKey, userInfo, soundName);
-    }
-
-    /**
-     * iOS
-     */
-    static setBadge(badge, cb) {
-        JPushModule.setBadge(badge, (value) => {
-            cb(value);
-        });
-    }
-
-    //  add listener
-    // NativeAppEventEmitter.addListener('networkDidSetup', (token) => {
-    //
-    // });
-    // NativeAppEventEmitter.addListener('networkDidClose', (token) => {
-    //
-    // });
-    // NativeAppEventEmitter.addListener('networkDidRegister', (token) => {
-    //
-    // });
-    // NativeAppEventEmitter.addListener('networkDidLogin', (token) => {
-    //
-    // });
+    },
 }
--- ios/RCTJPushModele/RCTJPushModule.h
+++ ios/RCTJPushModele/RCTJPushModule.h
@@ -1,13 +1,12 @@
 //
-//  JPushHelper.h
-//  PushDemo
+//  RCTJPushModule2.h
+//  RCTJMessage
 //
-//  Created by oshumini on 16/3/14.
-//  Copyright © 2016年 Facebook. All rights reserved.
+//  Created by user on 2017/6/4.
+//  Copyright © 2017年 xsdlr. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #elif __has_include("RCTBridgeModule.h")
@@ -15,15 +14,9 @@
 #elif __has_include("React/RCTBridgeModule.h")
 #import "React/RCTBridgeModule.h"
 #endif
-
-#import "JPUSHService.h"
-
-#define kJPFDidReceiveRemoteNotification  @"kJPFDidReceiveRemoteNotification"
-
-#define kJPFOpenNotification @"kJPFOpenNotification" // 通过点击通知 Notification 启动应用
-
-@interface RCTJPushModule : NSObject <RCTBridgeModule>
-@property(strong,nonatomic)RCTResponseSenderBlock asyCallback;
-
-- (void)didRegistRemoteNotification:(NSString *)token;
+@interface RCTJPushModule : NSObject<RCTBridgeModule>
++ (void)registerWithAppkey:(NSString *)appkey channel:(NSString *)channel launchOptions:(NSDictionary *)launchOptions withApp:(id)app;
++ (void)application:(UIApplication *)application didRegisterDeviceToken:(NSData *)deviceToken;
++ (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
++ (void)didReceiveRemoteNotificationWhenFirstLaunchApp:(NSDictionary *)launchOptions;
 @end
