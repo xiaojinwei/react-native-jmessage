2aeaeb3d77db276f3de6583a2e4a37adb06ba611
chenyongjian <yongjian.chen@b-and-qchina.com>
2017-07-13 15:58:21 +0800

    增加当前应用是否处于前台进程判断

--- android/src/main/java/com/xsdlr/rnjpush/DeviceUtil.java
+++ android/src/main/java/com/xsdlr/rnjpush/DeviceUtil.java
@@ -1,11 +1,14 @@
 package com.xsdlr.rnjpush;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.text.TextUtils;
+
+import java.util.List;
 
 import me.leolin.shortcutbadger.ShortcutBadger;
 
@@ -78,7 +81,7 @@ public class DeviceUtil {
     public static void applyCount(Context context) {
         int badgeCount = SharePreferencesUtil.getInt(context,BadgeCountKey,0);
         //极光会自动在通知发送一个通知，如果App不在最前面，角标会自动+1，这就是这时候就是等于小米可以跟极光已经配合做好角标了，不需要我们做什么，如果这时候我们再手动更新角标，则用户会收到两条通知，所以在发送之前需要判断一下是否小米手机
-        if (!isXiaoMi(context)) {
+        if (!isXiaoMi(context) && !isForeground(context)) {
             ShortcutBadger.applyCount(context, ++badgeCount); //for 1.1.4+
         }
         SharePreferencesUtil.saveInt(context,BadgeCountKey,badgeCount);
@@ -90,5 +93,25 @@ public class DeviceUtil {
     public static void removeCount(Context context) {
         SharePreferencesUtil.saveInt(context,BadgeCountKey,0);
         ShortcutBadger.removeCount(context); //for 1.1.4+
+    }
+
+    /**
+     * 判断当前应用是否处于前台
+     * @param context
+     * @return
+     */
+    private static boolean isForeground(Context context) {
+        if (context != null) {
+            ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo processInfo: processes) {
+                if (processInfo.processName.equals(context.getPackageName())) {
+                    if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
