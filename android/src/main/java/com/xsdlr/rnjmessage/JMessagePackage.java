aab7ae4156df836b7b8355a2e059bc9c8bfbd93e
user <guozhong.zhang@b-and-qchina.com>
2018-05-11 16:46:49 +0800

    RN版本升级兼容

--- android/src/main/java/com/xsdlr/rnjmessage/JMessagePackage.java
+++ android/src/main/java/com/xsdlr/rnjmessage/JMessagePackage.java
@@ -28,10 +28,6 @@ public class JMessagePackage implements ReactPackage {
         });
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
